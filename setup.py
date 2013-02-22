f3efda368b6a5cd483dbb0e015bf02c5454081b1
crccheck <c@crccheck.com>
2013-02-22 05:31:12 +0000

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="gspreadsheet",
-    version="0.1.0",
+    version="0.1.1",
     description="Google Spreadsheets the easy way",
     author='The Texas Tribune',
     author_email='cchang@texastribune.org',
